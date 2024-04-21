cdf7fa0866d3c6ec744f2ed5017fca6c32ef5763
Chealy-13 <chris.healy33@gmail.com>
2024-04-21 23:50:52 +0100

    BoundedPriorityQueue class start up,
    two fields added plus constructor holding docName and capacity of queue
    
    Created a "safe" add method

--- src/main/java/BoundedPriorityQueue.java
+++ src/main/java/BoundedPriorityQueue.java
@@ -7,6 +7,19 @@ public class BoundedPriorityQueue extends MyLinkedList{
         this.docName = docName;
     }
 
+    /**
+     * adds the appointment to the queue if queue is not full and
+     * if appointment is for doctor the queue belongs to
+     * @param appointment the appointment to be added to the queue
+     * @return true if the appointment is successfully added, false otherwise
+     */
+    @Override
+    public boolean add(Appointment appointment) {
+        if (size() >= capacity || !appointment.getDocName().equalsIgnoreCase(docName)) {
+            return false;  // Queue is full or appointment is for a different doctor
+        }
+        return super.add(appointment);
+    }
 
 }
 
